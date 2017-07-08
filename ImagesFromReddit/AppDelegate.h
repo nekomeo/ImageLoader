2542a58d544213020a8249482e9319dc1cabf996
nekomeo <linh.tu545@gmail.com>
2017-07-08 15:38:14 -0700

    Initial Commit

---
+++ ImagesFromReddit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImagesFromReddit
+//
+//  Created by Elle Ti on 2017-07-08.
+//  Copyright © 2017 Elle Ti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImagesFromReddit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImagesFromReddit
+//
+//  Created by Elle Ti on 2017-07-08.
+//  Copyright © 2017 Elle Ti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
